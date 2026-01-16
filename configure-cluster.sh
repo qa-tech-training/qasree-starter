386a1ff7def8ae1e752e7694ead3f51612e1afcb
qa-tech-training <agray@qa.com>
2026-01-16 08:26:53 +0000

    Update configure-cluster.sh

--- configure-cluster.sh
+++ configure-cluster.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-kubectl create ns splunk
-kubectl apply -f k8s/cluster_setup.yml
 git clone https://github.com/chaostoolkit-incubator/kubernetes-crd.git ctk-config
 kubectl apply -k ctk-config/manifests/overlays/generic-rbac
